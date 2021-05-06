4d3d4a936b2df7647bb165f87e8fa4e1ab533a4e
emilykidd3 <e.kidd61@yahoo.com>
2021-05-05 21:41:46 -0700

    replaced code with update version and add graphql

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -2,11 +2,10 @@ import React, { useEffect } from "react";
 import { useQuery } from '@apollo/react-hooks';
 
 import ProductItem from "../ProductItem";
+import { useStoreContext } from "../../utils/GlobalState";
+import { UPDATE_PRODUCTS } from "../../utils/actions";
 import { QUERY_PRODUCTS } from "../../utils/queries";
 import spinner from "../../assets/spinner.gif"
-
-import { useStoreContext } from '../../utils/GlobalState';
-import { UPDATE_PRODUCTS } from '../../utils/actions';
 
 function ProductList() {
   const [state, dispatch] = useStoreContext();
@@ -16,20 +15,20 @@ function ProductList() {
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
   useEffect(() => {
-    if (data) {
-      dispatch ({
-        type: UPDATE_PRODUCTS,
-        products: data.products
-      });
+    if(data) {
+      dispatch({
+           type: UPDATE_PRODUCTS,
+          products: data.products
+        });
     }
-  }, [data, dispatch])
+  }, [data, dispatch]);
 
   function filterProducts() {
     if (!currentCategory) {
       return state.products;
     }
 
-    return state.products.filter(product => product.category._id === currentCategory)
+    return state.products.filter(product => product.category._id === currentCategory);
   }
 
   return (
