46e6380f4e2e970d4dfdd0444765ca7189d8aecb
emilykidd3 <e.kidd61@yahoo.com>
2021-05-08 21:50:33 -0700

    updated resolvers to have checkout query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,6 +2,9 @@ const { AuthenticationError } = require('apollo-server-express');
 const { User, Product, Category, Order } = require('../models');
 const { signToken } = require('../utils/auth');
 
+// replace with env process.env.STRIPE_KEY
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc')
+
 const resolvers = {
   Query: {
     categories: async () => {
@@ -50,6 +53,10 @@ const resolvers = {
       }
 
       throw new AuthenticationError('Not logged in');
+    },
+    checkout: async (parent, args, context) => {
+      const order = new Order({ products: args.products });
+      const { products } = await order.populate('products').execPopulate();
     }
   },
   Mutation: {
